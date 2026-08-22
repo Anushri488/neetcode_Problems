c23249d7294fc072a622e7202500dd3d457697ae
Anushri Mishra <anushrimishra20@gmail.com>
2026-08-22 13:14:54 +0530

    Add: combination-target-sum - submission-1

---
+++ Data Structures & Algorithms/combination-target-sum/submission-1.java
@@ -0,0 +1,25 @@
+class Solution {
+    public void solve(int index,int target,List<Integer>current,List<List<Integer>>ans,int[]nums){
+        if(target==0){
+            ans.add(new ArrayList<>(current));
+            return;
+        }
+        if(target<0){
+            return;
+        }
+        if(index==nums.length){
+            return;
+        }
+        current.add(nums[index]);
+        solve(index,target-nums[index],current,ans,nums);
+        current.remove(current.size()-1);
+        solve(index+1,target,current,ans,nums);
+    }
+    public List<List<Integer>> combinationSum(int[] nums, int target) {
+        List<Integer>current=new ArrayList<>();
+        List<List<Integer>>ans= new ArrayList<>();
+        solve(0,target,current,ans,nums);
+        return ans;
+    }
+}     
+        
